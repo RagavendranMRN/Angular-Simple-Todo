26f8ad475f0599e47f48234f297989cfafb5cf8a
RagavendranMRN <ragavendran1371997@gmail.com>
2021-07-17 20:29:25 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION, Input } from '@angular/core';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'my-app',
@@ -12,6 +13,8 @@ export class AppComponent {
   newTask: any;
   filterBy: String = 'All';
 
+  constructor(private toastr: ToastrService) {}
+
   _handleTaskAdd() {
     let Taskobj = {
       id: this.tasks.length,
@@ -23,17 +26,21 @@ export class AppComponent {
     };
     this.tasks.push(Taskobj);
     this.Filteredtasks = this.tasks.filter(v => v.tags === this.filterBy);
+    // this.toastr.success(Taskobj.task + ' is Created successfully');
     this.newTask = null;
   }
   _handleDeleteTask(value) {
     this.tasks = this.tasks.filter(v => v !== value);
     this.Filteredtasks = this.tasks.filter(v => v.tags === this.filterBy);
+    // this.toastr.info(value.task + ' is removed successfully');
   }
   _handleCompletedTask(value) {
     let index = this.tasks.findIndex(v => v.id == value);
     this.tasks[index].isCompleted = true;
     this.tasks[index].tags = 'Completed';
     this.Filteredtasks = this.tasks.filter(v => v.tags === this.filterBy);
+
+    // this.toastr.info(this.tasks[index].task + ' is Completed');
   }
 
   _handleFilter(filter) {
